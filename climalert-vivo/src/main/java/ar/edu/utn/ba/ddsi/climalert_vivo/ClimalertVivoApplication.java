bb8464845e359360e9f10819ff2b9e21f5c86b80
Lucilapacheco <lpacheco@frba.utn.edu.ar>
2026-07-06 21:01:37 -0300

    Primer commit

---
+++ climalert-vivo/src/main/java/ar/edu/utn/ba/ddsi/climalert_vivo/ClimalertVivoApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.ba.ddsi.climalert_vivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimalertVivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimalertVivoApplication.class, args);
+	}
+
+}
